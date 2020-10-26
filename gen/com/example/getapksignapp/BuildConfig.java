2d696cc8e42c67ad9d12871422e3a78d6ef46c4b
fujie <1243596620@qq.com>
2020-10-26 21:39:38 +0800

    first commit

---
+++ gen/com/example/getapksignapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.getapksignapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/getapksignapp/SignUtils.java
@@ -0,0 +1,73 @@
+package com.example.getapksignapp;
+
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.DisplayMetrics;
+
+public class SignUtils {
+
+
+	public static String showUninstallAPKSignatures(String apkPath) {
+		String PATH_PackageParser = "android.content.pm.PackageParser";
+		try {
+			// apk包的文件路径
+			// 这是一个Package 解释器, 是隐藏的
+			// 构造函数的参数只有一个, apk文件的路径
+			// PackageParser packageParser = new PackageParser(apkPath);
+			Class pkgParserCls = Class.forName(PATH_PackageParser);
+			Class[] typeArgs = new Class[1];
+			typeArgs[0] = String.class;
+			Constructor pkgParserCt = pkgParserCls.getConstructor(typeArgs);
+			Object[] valueArgs = new Object[1];
+			valueArgs[0] = apkPath;
+			Object pkgParser = pkgParserCt.newInstance(valueArgs);
+//			MediaApplication.logD(DownloadApk.class,
+//					"pkgParser:" + pkgParser.toString());
+			// 这个是与显示有关的, 里面涉及到一些像素显示等等, 我们使用默认的情况
+			DisplayMetrics metrics = new DisplayMetrics();
+			metrics.setToDefaults();
+			// PackageParser.Package mPkgInfo = packageParser.parsePackage(new
+			// File(apkPath), apkPath,
+			// metrics, 0);
+			typeArgs = new Class[4];
+			typeArgs[0] = File.class;
+			typeArgs[1] = String.class;
+			typeArgs[2] = DisplayMetrics.class;
+			typeArgs[3] = Integer.TYPE;
+			Method pkgParser_parsePackageMtd = pkgParserCls.getDeclaredMethod(
+					"parsePackage", typeArgs);
+			valueArgs = new Object[4];
+			valueArgs[0] = new File(apkPath);
+			valueArgs[1] = apkPath;
+			valueArgs[2] = metrics;
+			valueArgs[3] = PackageManager.GET_SIGNATURES;
+			Object pkgParserPkg = pkgParser_parsePackageMtd.invoke(pkgParser,
+					valueArgs);
+
+			typeArgs = new Class[2];
+			typeArgs[0] = pkgParserPkg.getClass();
+			typeArgs[1] = Integer.TYPE;
+			Method pkgParser_collectCertificatesMtd = pkgParserCls
+					.getDeclaredMethod("collectCertificates", typeArgs);
+			valueArgs = new Object[2];
+			valueArgs[0] = pkgParserPkg;
+			valueArgs[1] = PackageManager.GET_SIGNATURES;
+			pkgParser_collectCertificatesMtd.invoke(pkgParser, valueArgs);
+			// 应用程序信息包, 这个公开的, 不过有些函数, 变量没公开
+			Field packageInfoFld = pkgParserPkg.getClass().getDeclaredField(
+					"mSignatures");
+			Signature[] info = (Signature[]) packageInfoFld.get(pkgParserPkg);
+//			MediaApplication.logD(DownloadApk.class, "size:" + info.length);
+//			MediaApplication.logD(DownloadApk.class, info[0].toCharsString());
+			return info[0].toCharsString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
